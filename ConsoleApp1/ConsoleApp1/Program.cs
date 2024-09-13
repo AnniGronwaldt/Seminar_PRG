424039ec883d2e84fa1cdfaf837afa472af09c8a
Gronwaldtova Anna <gronwaldtova@gymvod.local>
2024-09-13 09:35:15 +0200

    added first Console App

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            float b = 7.1f;
+            float result = a / b;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
