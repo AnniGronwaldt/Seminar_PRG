c934719cdcc3464b39a585b92ab7964276cc6570
AnniGronwaldt <anna.grona@seznam.cz>
2024-11-07 21:43:40 +0100

    Add files via upload

---
+++ VolebniPocitadlo/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Dictionary<string, int> elections = new Dictionary<string, int>();
+
+elections["Trump"] = 0;
+elections["Harris"] = 0;
+elections["West"] = 0;
+elections["Stein"] = 0;
+elections["Oliver"] = 0;
+
+
+
+foreach (var   in args)
+{
+
+
+    Console.WriteLine("Trump: " + elections["Trump"] + "vs" + "Harris: " + elections["Harris"] + "vs" + "West: " + elections["West"] + "vs" + "Stein: " + elections["Stein"] + "vs" + "Oliver: " + elections["Oliver"]);
+}
+
